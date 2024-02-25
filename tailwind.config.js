997c4b4f049cf3028d99050d09352562cb7db8aa
bahartika <bahartikasatria271@gmail.com>
2024-02-25 18:06:35 +0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./build/index.html", "./**/*.js"],
   darkMode: "class",
   theme: {
     extend: {},
